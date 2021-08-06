3327bd1093f83dd0f3ab63d7b84c2acc249d9156
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2021-08-06 18:02:26 -0300

    add tmp folder

--- src/utils/clear-tmp.ts
+++ src/utils/clear-tmp.ts
@@ -1,8 +1,12 @@
-import { readdirSync, unlinkSync } from "fs";
+import { existsSync, mkdirSync, readdirSync, unlinkSync } from "fs";
 import { resolve, join } from "path";
 
 export default () => {
   const directory = resolve(__dirname, "..", "tmp");
+
+  if (!existsSync(directory)) {
+    mkdirSync(directory);
+  }
 
   const files = readdirSync(directory);
   for (const file of files) {
